f0d383540a8448fef69b2297adbb5ff90cd1190c
denhampreen <denhampreen@gmail.com>
2022-09-12 16:00:36 +0200

    validate address type and more tests

--- templates/demo-subgraph/src/token-lock-wallet.ts
+++ templates/demo-subgraph/src/token-lock-wallet.ts
@@ -1,4 +1,4 @@
-import { BigInt, dataSource } from '@graphprotocol/graph-ts'
+import { BigInt, dataSource } from "@graphprotocol/graph-ts";
 import {
   TokensReleased,
   TokensWithdrawn,
@@ -6,49 +6,96 @@ import {
   ManagerUpdated,
   ApproveTokenDestinations,
   RevokeTokenDestinations,
-} from '../generated/templates/GraphTokenLockWallet/GraphTokenLockWallet'
+} from "../generated/templates/GraphTokenLockWallet/GraphTokenLockWallet";
 
-import { TokenLockWallet } from '../generated/schema'
+import { TokenLockWallet } from "../generated/schema";
 
+import {
+  getTokenLockWallet,
+  generateTokenLockWalletId,
+  setTokensReleased,
+  setTokensWithdrawn,
+  setTokensRevoked,
+  setTokenDestinationsApproved,
+} from "./generated/UncrashableHelpers";
+
+// export function handleTokensReleased(event: TokensReleased): void {
+//   let tokenLockWallet = TokenLockWallet.load(event.address.toHexString())!
+//   tokenLockWallet.tokensReleased = tokenLockWallet.tokensReleased.plus(event.params.amount)
+//   tokenLockWallet.save()
+// }
+
+// export function handleTokensWithdrawn(event: TokensWithdrawn): void {
+//   let tokenLockWallet = TokenLockWallet.load(event.address.toHexString())!
+//   tokenLockWallet.tokensWithdrawn = tokenLockWallet.tokensWithdrawn.plus(event.params.amount)
+//   tokenLockWallet.save()
+// }
+
+// export function handleTokensRevoked(event: TokensRevoked): void {
+//   let tokenLockWallet = TokenLockWallet.load(event.address.toHexString())!
+//   tokenLockWallet.tokensRevoked = tokenLockWallet.tokensRevoked.plus(event.params.amount)
+//   tokenLockWallet.save()
+// }
 export function handleTokensReleased(event: TokensReleased): void {
-  let tokenLockWallet = TokenLockWallet.load(event.address.toHexString())!
-  tokenLockWallet.tokensReleased = tokenLockWallet.tokensReleased.plus(event.params.amount)
-  tokenLockWallet.save()
+  let tokenLockWallet = getTokenLockWallet(
+    generateTokenLockWalletId(event.address)
+  );
+  setTokensReleased(event.address.toHexString(), {
+    tokensReleased: tokenLockWallet.tokensReleased.plus(event.params.amount),
+  });
 }
 
 export function handleTokensWithdrawn(event: TokensWithdrawn): void {
-  let tokenLockWallet = TokenLockWallet.load(event.address.toHexString())!
-  tokenLockWallet.tokensWithdrawn = tokenLockWallet.tokensWithdrawn.plus(event.params.amount)
-  tokenLockWallet.save()
+  let tokenLockWallet = getTokenLockWallet(
+    generateTokenLockWalletId(event.address)
+  );
+  setTokensWithdrawn(event.address.toHexString(), {
+    tokensWithdrawn: tokenLockWallet.tokensWithdrawn.plus(event.params.amount),
+  });
 }
 
 export function handleTokensRevoked(event: TokensRevoked): void {
-  let tokenLockWallet = TokenLockWallet.load(event.address.toHexString())!
-  tokenLockWallet.tokensRevoked = tokenLockWallet.tokensRevoked.plus(event.params.amount)
-  tokenLockWallet.save()
+  let tokenLockWallet = getTokenLockWallet(
+    generateTokenLockWalletId(event.address)
+  );
+  setTokensRevoked(event.address.toHexString(), {
+    tokensRevoked: tokenLockWallet.tokensRevoked.plus(event.params.amount),
+  });
 }
 
 export function handleManagerUpdated(event: ManagerUpdated): void {
-  let id = event.address.toHexString()
-  let tokenLockWallet = TokenLockWallet.load(id)!
-  tokenLockWallet.manager = event.params._newManager
-  tokenLockWallet.save()
+  let id = event.address.toHexString();
+  let tokenLockWallet = TokenLockWallet.load(id)!;
+  tokenLockWallet.manager = event.params._newManager;
+  tokenLockWallet.save();
 }
 
-export function handleApproveTokenDestinations(event: ApproveTokenDestinations): void {
-  let tokenLockWallet = TokenLockWallet.load(dataSource.address().toHexString())!
+export function handleApproveTokenDestinations(
+  event: ApproveTokenDestinations
+): void {
+  let tokenLockWallet = getTokenLockWallet(
+    generateTokenLockWalletId(dataSource.address())
+  );
   if (dataSource.network() == "rinkeby") {
-    tokenLockWallet.tokenDestinationsApproved = true
+    setTokenDestinationsApproved(
+      generateTokenLockWalletId(dataSource.address()),
+      { tokenDestinationsApproved: true }
+    );
   }
-  let context = dataSource.context()
+  let context = dataSource.context();
   if (context.get("contextVal")!.toI32() > 0) {
-    tokenLockWallet.setBigInt("tokensReleased", BigInt.fromI32(context.get("contextVal")!.toI32()))
+    tokenLockWallet.setBigInt(
+      "tokensReleased",
+      BigInt.fromI32(context.get("contextVal")!.toI32())
+    );
   }
-  tokenLockWallet.save()
+  tokenLockWallet.save();
 }
 
-export function handleRevokeTokenDestinations(event: RevokeTokenDestinations): void {
-  let tokenLockWallet = TokenLockWallet.load(event.address.toHexString())!
-  tokenLockWallet.tokenDestinationsApproved = false
-  tokenLockWallet.save()
+export function handleRevokeTokenDestinations(
+  event: RevokeTokenDestinations
+): void {
+  let tokenLockWallet = TokenLockWallet.load(event.address.toHexString())!;
+  tokenLockWallet.tokenDestinationsApproved = false;
+  tokenLockWallet.save();
 }
--- templates/demo-subgraph/tests/token-lock-wallet/token-lock-wallet.test.ts
+++ templates/demo-subgraph/tests/token-lock-wallet/token-lock-wallet.test.ts
@@ -1,106 +1,130 @@
-import { assert, test, newMockEvent, dataSourceMock, describe, beforeAll, afterAll, logStore, clearStore } from "matchstick-as/assembly/index"
-import { Address, BigInt, Bytes, DataSourceContext, store, Value } from "@graphprotocol/graph-ts"
+import {
+  assert,
+  test,
+  newMockEvent,
+  dataSourceMock,
+  describe,
+  beforeAll,
+  afterAll,
+  logStore,
+  clearStore,
+} from "matchstick-as/assembly/index";
+import {
+  Address,
+  BigInt,
+  Bytes,
+  DataSourceContext,
+  store,
+  Value,
+} from "@graphprotocol/graph-ts";
 
-import { handleApproveTokenDestinations } from "../../src/token-lock-wallet"
-import { ApproveTokenDestinations } from "../../generated/templates/GraphTokenLockWallet/GraphTokenLockWallet"
-import { NameSignalTransaction, TokenLockWallet, GraphAccount, Subgraph } from "../../generated/schema"
-import { mockGraphAccount, mockNameSignalTransaction } from "./utils"
+import { handleApproveTokenDestinations } from "../../src/token-lock-wallet";
+import { ApproveTokenDestinations } from "../../generated/templates/GraphTokenLockWallet/GraphTokenLockWallet";
+import {
+  NameSignalTransaction,
+  TokenLockWallet,
+  GraphAccount,
+  Subgraph,
+} from "../../generated/schema";
+import { mockGraphAccount, mockNameSignalTransaction } from "./utils";
 
 describe("dataSourceMock", () => {
   beforeAll(() => {
-    let addressString = "0xA16081F360e3847006dB660bae1c6d1b2e17eC2A"
-    let address = Address.fromString(addressString)
+    let addressString = "0xA16081F360e3847006dB660bae1c6d1b2e17eC2A";
+    let address = Address.fromString(addressString);
 
-    let wallet = new TokenLockWallet(address.toHexString())
+    let wallet = new TokenLockWallet(address.toHexString());
     // The following values should be set, because they are required fields,
     // Since graph-cli 0.26.1, 0.27.1, 0.28.2 and 0.29.1, graph codegen will not generate
     // default values for the required fields on .save()
-    wallet.manager = Address.zero()
-    wallet.initHash = Address.zero() as Bytes
-    wallet.beneficiary = Address.zero()
-    wallet.tokenDestinationsApproved = false
-    wallet.token = Bytes.fromHexString("0xc944e90c64b2c07662a292be6244bdf05cda44a7") // GRT
-    wallet.managedAmount = BigInt.fromI32(0)
-    wallet.startTime = BigInt.fromI32(0)
-    wallet.endTime = BigInt.fromI32(0)
-    wallet.periods = BigInt.fromI32(0)
-    wallet.releaseStartTime = BigInt.fromI32(0)
-    wallet.vestingCliffTime = BigInt.fromI32(0)
-    wallet.tokensReleased = BigInt.fromI32(0)
-    wallet.tokensWithdrawn = BigInt.fromI32(0)
-    wallet.tokensRevoked = BigInt.fromI32(0)
-    wallet.blockNumberCreated = BigInt.fromI32(0)
-    wallet.txHash = Address.zero() as Bytes
+    wallet.manager = Address.zero();
+    wallet.initHash = Address.zero() as Bytes;
+    wallet.beneficiary = Address.zero();
+    wallet.tokenDestinationsApproved = false;
+    wallet.token = Bytes.fromHexString(
+      "0xc944e90c64b2c07662a292be6244bdf05cda44a7"
+    ); // GRT
+    wallet.managedAmount = BigInt.fromI32(0);
+    wallet.startTime = BigInt.fromI32(0);
+    wallet.endTime = BigInt.fromI32(0);
+    wallet.periods = BigInt.fromI32(0);
+    wallet.releaseStartTime = BigInt.fromI32(0);
+    wallet.vestingCliffTime = BigInt.fromI32(0);
+    wallet.tokensReleased = BigInt.fromI32(0);
+    wallet.tokensWithdrawn = BigInt.fromI32(0);
+    wallet.tokensRevoked = BigInt.fromI32(0);
+    wallet.blockNumberCreated = BigInt.fromI32(0);
+    wallet.txHash = Address.zero() as Bytes;
 
-    wallet.save()
+    wallet.save();
 
-    let context = new DataSourceContext()
-    context.set("contextVal", Value.fromI32(325))
+    let context = new DataSourceContext();
+    context.set("contextVal", Value.fromI32(325));
 
-    dataSourceMock.setReturnValues(addressString, "rinkeby", context)
-  })
+    dataSourceMock.setReturnValues(addressString, "rinkeby", context);
+  });
 
   afterAll(() => {
-    dataSourceMock.resetValues()
-  })
+    dataSourceMock.resetValues();
+  });
 
   test("Simple dataSource mocking example", () => {
-    let addressString = "0xA16081F360e3847006dB660bae1c6d1b2e17eC2A"
-    let address = Address.fromString(addressString)
+    let addressString = "0xA16081F360e3847006dB660bae1c6d1b2e17eC2A";
+    let address = Address.fromString(addressString);
 
-    let event = changetype<ApproveTokenDestinations>(newMockEvent())
-    let wallet = TokenLockWallet.load(address.toHexString())!
+    let event = changetype<ApproveTokenDestinations>(newMockEvent());
+    let wallet = TokenLockWallet.load(address.toHexString())!;
 
-    assert.assertTrue(!wallet.tokenDestinationsApproved)
+    assert.assertTrue(!wallet.tokenDestinationsApproved);
 
-    handleApproveTokenDestinations(event)
+    handleApproveTokenDestinations(event);
 
-    wallet = TokenLockWallet.load(address.toHexString())!
+    wallet = TokenLockWallet.load(address.toHexString())!;
 
-    assert.assertTrue(wallet.tokenDestinationsApproved)
-    assert.bigIntEquals(wallet.tokensReleased, BigInt.fromI32(325))
-  })
-})
+    assert.assertTrue(wallet.tokenDestinationsApproved);
+    assert.bigIntEquals(wallet.tokensReleased, BigInt.fromI32(325));
+  });
+});
 
 describe("@derivedFrom fields", () => {
   beforeAll(() => {
-    mockGraphAccount("12")
-    mockGraphAccount("1")
-  })
+    mockGraphAccount("12");
+    mockGraphAccount("1");
+  });
 
   afterAll(() => {
-    clearStore()
-  })
+    clearStore();
+  });
 
   test("Derived fields example test", () => {
-    let mainAccount = GraphAccount.load("12")!
+    let mainAccount = GraphAccount.load("12")!;
 
-    assert.assertNull(mainAccount.get("nameSignalTransactions"))
-    assert.assertNull(mainAccount.get("operatorOf"))
+    assert.assertNull(mainAccount.get("nameSignalTransactions"));
+    assert.assertNull(mainAccount.get("operatorOf"));
 
-    let operatedAccount = GraphAccount.load("1")!
-    operatedAccount.operators = [mainAccount.id]
-    operatedAccount.save()
+    let operatedAccount = GraphAccount.load("1")!;
+    operatedAccount.operators = [mainAccount.id];
+    operatedAccount.save();
 
-    mockNameSignalTransaction("1234", mainAccount.id)
-    mockNameSignalTransaction("2", mainAccount.id)
+    mockNameSignalTransaction("1234", mainAccount.id);
+    mockNameSignalTransaction("2", mainAccount.id);
 
-    mainAccount = GraphAccount.load("12")!
+    mainAccount = GraphAccount.load("12")!;
 
-    assert.assertNotNull(mainAccount.get("nameSignalTransactions"))
-    assert.assertNotNull(mainAccount.get("operatorOf"))
-    assert.i32Equals(2, mainAccount.nameSignalTransactions.length)
-    assert.stringEquals("1", mainAccount.operatorOf[0])
+    assert.assertNotNull(mainAccount.get("nameSignalTransactions"));
+    assert.assertNotNull(mainAccount.get("operatorOf"));
+    assert.i32Equals(2, mainAccount.nameSignalTransactions.length);
+    assert.stringEquals("1", mainAccount.operatorOf[0]);
 
-    mockNameSignalTransaction("2345", mainAccount.id)
+    mockNameSignalTransaction("2345", mainAccount.id);
 
-    let nst = NameSignalTransaction.load("1234")!
-    nst.signer = "11"
-    nst.save()
+    let nst = NameSignalTransaction.load("1234")!;
+    nst.signer = "11";
+    nst.save();
 
-    store.remove("NameSignalTransaction", "2")
+    store.remove("NameSignalTransaction", "2");
 
-    mainAccount = GraphAccount.load("12")!
-    assert.i32Equals(1, mainAccount.nameSignalTransactions.length)
-  })
-})
+    mainAccount = GraphAccount.load("12")!;
+    assert.i32Equals(1, mainAccount.nameSignalTransactions.length);
+  });
+});
