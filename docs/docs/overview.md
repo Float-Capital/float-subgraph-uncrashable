7b7e850c13e7c54595125ba5d1b8c6c22890d634
Sven Mueller <svenmueller@Svens-MacBook-Air.local>
2022-09-12 13:32:32 +0200

    images on landing site (excl custom logos) and docs content. Mike doing 'Installation' and 'Configuration' docs. Sven did 'Welcome' 'Overview' and 'Support'

--- docs/docs/overview.md
+++ docs/docs/overview.md
@@ -9,3 +9,14 @@ slug: /overview
 
 ---
 
+**Subgraph Uncrashable by Float** is a code generation tool that generates a set of helper functions from the graphql scheme of the project. 
+
+This completely abstracts the need for subgraph developers to call `new Entity`, `Entity.load` and `Entity.save()` functions which are all inherently error prone, and in turn ensures that all interactions with entities in the subgraph are completely safe. 
+
+Please note, this doesn’t mean that the subgraph code won’t have logic errors. Sometimes it is useful for a subgraph to crash when things appear to go wrong (e.g. while it is syncing historic data), other times you want it to keep going but alert the development team that there is an issue so they can rectify things as soon as possible. We have gone with the approach of allowing the subgraph developer to specify a timestamp after which the subgraph should no-longer crash on error but rather keep going using our default entity approach.
+
+The code generation tool accommodates all subgraph types and is configurable for users to set  sane defaults on values. The code generation will use this config to generate helper functions that are to the users specification.
+
+The framework also includes a way (via the config file) to create custom but safe setter functions for groups of entity variables. This way it is impossible for the user to load/use a stale graph entity and it is also impossible to forget to save or set a variable that is required by the function.
+
+Warning logs are recorded as warning logs indicating where there is a breach of subgraph logic to help patch the issue to ensure data accuracy. These logs can be viewed in the The Graph's hosted service under the 'Logs' section. 
--- docs/src/components/HomepageFeatures/index.js
+++ docs/src/components/HomepageFeatures/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import clsx from 'clsx';
 import styles from './styles.module.css';
 
+
 const FeatureList = [
   {
     title: 'Continuous Uptime',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    // Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    Png: require('@site/static/img/rocket.png').default,
+
     description: (
       <>
         Mishandled entities cause subgraphs to crash, which can be very disruptive for projects that are dependent on the graph. Set up helper functions to make your subgraphs “uncrashable” and ensure business continuity. 
@@ -14,7 +17,8 @@ const FeatureList = [
   },
   {
     title: 'Completely Safe',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    // Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    Png: require('@site/static/img/sun.png').default,
     description: (
       <>
         Common problems seen in subgraph development are issues of loading undefined entities, not setting or initializing all values of entities, and race conditions on loading and saving entities. Ensure all interactions with entities are completely atomic. 
@@ -23,20 +27,22 @@ const FeatureList = [
   },
   {
     title: 'User Configurable',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    // Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    Png: require('@site/static/img/asteroid.png').default,
     description: (
       <>
-        Set default values and configure the level of security checks that suits your individual project's needs. Warning logs are recorded indicating where there is a breach of subgraph logic tp help patch the issue to ensure data accuracy. 
+        Set default values and configure the level of security checks that suits your individual project's needs. Warning logs are recorded indicating where there is a breach of subgraph logic to help patch the issue to ensure data accuracy. 
       </>
     ),
   },
 ];
 
-function Feature({Svg, title, description}) {
+function Feature({Png, Svg, title, description}) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
+        {/* <Svg className={styles.featureSvg} role="img" /> */}
+      <img src={Png} className={styles.featureImg}/>
       </div>
       <div className="text--center padding-horiz--md">
         <h3>{title}</h3>
